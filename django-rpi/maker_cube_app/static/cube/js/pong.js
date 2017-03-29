0a3b86b3ccf5254792b9c30cc377766a9aa315ef
Rob Adams <robert.andrew.adams@gmail.com>
2017-03-29 12:01:29 +0100

    removed quote from div name

--- django-rpi/maker_cube_app/static/cube/js/pong.js
+++ django-rpi/maker_cube_app/static/cube/js/pong.js
@@ -258,7 +258,7 @@ mainState.prototype = {
 };
 
 createGame = function(gameDiv) {
-  game = new Phaser.Game(gameProperties.screenWidth, gameProperties.screenHeight, Phaser.AUTO, 'gameDiv');
+  game = new Phaser.Game(gameProperties.screenWidth, gameProperties.screenHeight, Phaser.AUTO, gameDiv);
   game.state.add('main', mainState);
   game.state.start('main');
 }
